56a7e92945aa3f676ed96d936c38ac650ca54002
Josh Cough <joshcough@gmail.com>
2025-03-18 21:18:55 -0400

    Some claude cleanups

--- src/components/BreedComponent.purs
+++ src/components/BreedComponent.purs
@@ -1,68 +1,78 @@
 module Components.BreedComponent (renderBreedComponent) where
 
 import Prelude
+
 import Cache (initCache)
-import Components.BreedList (renderBreedList)
 import Components.BreedDetails (renderBreedDetails)
+import Components.BreedList (renderBreedList)
+import Data.Maybe (maybe)
+import Data.String (Pattern(..), indexOf, take, drop)
 import DogsApi (Breed(..))
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Data.Maybe (maybe)
-import Data.String (Pattern(..), indexOf, take, drop)
 import Web.DOM.Document (Document, createElement)
-import Web.DOM.Node (appendChild, setTextContent)
 import Web.DOM.Element (Element, toNode)
+import Web.DOM.Node (appendChild, setTextContent)
 
--- App states
+-- | Application state representation
 data AppState
   = BreedListState
   | BreedDetailsState Breed
 
+-- | Main component renderer
 renderBreedComponent :: Document -> Element -> Effect Unit
 renderBreedComponent doc container = do
   -- Initialize the cache
   cacheRef <- liftEffect initCache
-  -- Create app container
+
+  -- Create DOM elements
   appContainer <- createElement "div" doc
-  -- Create header
   heading <- createElement "h1" doc
-  setTextContent "Dog Breeds Explorer" (toNode heading)
-  -- Main content container
   contentContainer <- createElement "div" doc
-  -- Append header elements
-  let
-    containerNode = toNode container
+
+  -- Set content
+  setTextContent "Dog Breeds Explorer" (toNode heading)
+
+  -- Build DOM structure
+  let containerNode = toNode container
   _ <- appendChild (toNode heading) containerNode
   _ <- appendChild (toNode appContainer) containerNode
   _ <- appendChild (toNode contentContainer) (toNode appContainer)
-  -- Define state transition functions
+
+  -- Set up state transitions
   let
-    -- Function to handle breed selection
+    -- Handle breed selection
     onBreedSelect :: String -> Effect Unit
     onBreedSelect breedStr = do
       log ("Selected breed: " <> breedStr)
-      let
-        slashIndex = indexOf (Pattern "/") breedStr
+      renderApp (BreedDetailsState (parseBreed breedStr))
 
-        breed =
-          Breed
-            { name: maybe breedStr (\ix -> take ix breedStr) slashIndex
-            , subBreed: map (\ix -> drop (ix + 1) breedStr) slashIndex
-            }
-      renderApp (BreedDetailsState breed)
-
-    -- Function to handle going back to breed list
+    -- Handle back navigation
     onBackToList :: Effect Unit
     onBackToList = do
       log "Going back to breed list"
       renderApp BreedListState
 
-    -- Function to render the app based on state
+    -- Parse breed string into Breed data structure
+    parseBreed :: String -> Breed
+    parseBreed breedStr =
+      let
+        slashIndex = indexOf (Pattern "/") breedStr
+      in
+        Breed
+          { name: maybe breedStr (\ix -> take ix breedStr) slashIndex
+          , subBreed: map (\ix -> drop (ix + 1) breedStr) slashIndex
+          }
+
+    -- Render application based on current state
     renderApp :: AppState -> Effect Unit
-    renderApp state = case state of
-      BreedListState -> renderBreedList doc contentContainer onBreedSelect cacheRef
-      BreedDetailsState breed -> renderBreedDetails doc contentContainer onBackToList breed cacheRef
-  -- Initial render - start with breed list
+    renderApp = case _ of
+      BreedListState ->
+        renderBreedList doc contentContainer onBreedSelect cacheRef
+      BreedDetailsState breed ->
+        renderBreedDetails doc contentContainer onBackToList breed cacheRef
+
+  -- Initial render with breed list
   renderApp BreedListState
   log "Application initialized"
